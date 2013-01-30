6a1793d6c89143ba1814edc0c784c49cd4df0541
Jeffrey Horn <jrhorn424@gmail.com>
2013-01-30 03:43:23 -0500

    Clear out some style

--- style/ices/html_header.php
+++ style/ices/html_header.php
@@ -1,44 +1,11 @@
 <center>
 <TABLE BORDER=0 CELLSPACING=0 CELLPADDING=0 WIDTH="100%">
-	<TR VALIGN=TOP ALIGN=LEFT BGCOLOR="#BC8F8F">
+	<TR VALIGN=TOP ALIGN=LEFT BGCOLOR="#FFF">
 		<TD COLSPAN=2 WIDTH=100% HEIGHT=19 ALIGN=RIGHT VALIGN=BOTTOM>
 		<P>ICES Recruiter</P>
 		</TD>
 	</TR>
-
-
-	<TR bgcolor="#2F4F4F">
-		<TD WIDTH=100% HEIGHT=70 VALIGN=MIDDLE ALIGN=LEFT rowspan=2>
-			<TABLE BORDER=0 CELLSPACING=0 CELLPADDING=0 WIDTH=300>
-			<TR>
-				<TD VALIGN=TOP>
-				&nbsp;&nbsp;&nbsp;| &#248;
-				</TD>
-			</TR>
-			</TABLE>
-		</TD>
-                <TD ALIGN=RIGHT VALIGN=bottom WIDTH="100%">
-                        <TABLE BORDER=0 CELLSPACING=0 CELLPADDING=0 WIDTH="100%" HEIGHT="100%">
-
-
-                        <TR>
-                                <TD valign=bottom align=right>
-                                &nbsp;&nbsp;&nbsp;
-                                </TD>
-                        </TR>
-                        </TABLE>
-                </TD>
-
-	</TR>
-
-	<TR bgcolor="#2F4F4F">
-                <TD valign=top align=right>
-                  
-                </TD>
-
-	</TR>
-
-	<TR bgcolor="#BC8F8F" valign=top align=center>
+	<TR bgcolor="#FFF" valign=top align=center>
 		<TD valign=top align=left colspan=2>
 		<?php navigation("horizontal",false); ?>
 		</TD>
