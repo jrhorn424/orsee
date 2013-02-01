bf02db8276a9bc66421567652300e3a765df1159
Jeffrey Horn <jrhorn424@gmail.com>
2013-02-01 04:09:25 -0500

    Hide contact; section out nav

--- public/navigation.php
+++ public/navigation.php
@@ -9,11 +9,13 @@ link|mainpage|mainpage|/public/index.php|||true
 
 link|public_register|register|/public/participant_create.php|||||true
 
+//Participant section
 link|my_data|my_data|/public/participant_edit.php|||true|true
 link|my_registrations|my_registrations|/public/participant_show.php|||true|true
 
 //link|calendar|calendar|/public/show_calendar.php|||true
 
+// Addp
 link|rules|rules|/public/rules.php|||true
 
 link|privacy|privacy_policy|/public/privacy.php|||true
@@ -23,7 +25,7 @@ link|faqs|faqs|/public/faq.php|||true
 //headlink|internet_exp|internet_experiments|/public/ie.php|||true||true
 
 head|||
-link||impressum|/public/directions.php
-link|contact|contact|/public/contact.php|||true
+link||impressum|/public/directions.php|||true
+//link|contact|contact|/public/contact.php|||true
 
 link||login|/admin/admin_login.php
