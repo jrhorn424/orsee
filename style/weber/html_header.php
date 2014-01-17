04cc35029656d3ba645614c0edd5ff060d08ad16
Jeffrey Horn <jrhorn424@gmail.com>
2014-01-16 23:23:49 -0500

    Add weber style

---
+++ style/weber/html_header.php
@@ -0,0 +1,21 @@
+<center>
+<TABLE BORDER=0 CELLSPACING=0 CELLPADDING=0 WIDTH="100%">
+	<TR VALIGN=TOP ALIGN=LEFT BGCOLOR="#FFF">
+		<TD COLSPAN=2 WIDTH=100% HEIGHT=19 ALIGN=RIGHT VALIGN=BOTTOM>
+		<IMG border=0 SRC="../style/weber/weber-logo.gif">
+		</TD>
+	</TR>
+	<TR bgcolor="#FFF" valign=top align=center>
+		<TD valign=top align=left colspan=2>
+		<?php navigation("horizontal",false); ?>
+		</TD>
+	</TR>
+
+	<TR>
+		<TD VALIGN=TOP ALIGN=LEFT colspan=2>
+
+			<TABLE BORDER=0 CELLSPACING=0 CELLPADDING=0 WIDTH="100%" HEIGHT="100%">
+			<TR>
+
+			<TD width=100% valign=top>
+
