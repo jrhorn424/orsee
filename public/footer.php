54ced13c0aaeac78f79b9c6c36cbed793c0a562d
Jeffrey Horn <jrhorn424@gmail.com>
2014-01-28 06:42:09 -0500

    Remove public footer

--- public/footer.php
+++ public/footer.php
@@ -1,16 +1,4 @@
-<?php 
-
-echo '
-<center>
-<BR><BR><BR>
-<P class="small" align=center>';
-echo $lang['for_questions_contact_xxx']; 
-echo ' <A class="small" HREF="mailto:';
-echo $settings['support_mail'].'">'.icon('email').' '.$settings['support_mail'];
-echo '</A>.<BR>
-	<BR><BR>
-
-	</CENTER>';
+<?php
 
 include ("../style/".$settings['style']."/html_footer.php");
 html__footer();
