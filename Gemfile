fc5c755693fec751de5d0a23ba3bc0bbf1e2c64c
Jeffrey Horn <jrhorn424@gmail.com>
2014-01-31 18:03:38 -0500

    Add mina-scp for managing secrets

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "https://rubygems.org"
 
 gem 'mina'
+gem 'mina-scp'
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,11 @@
-require 'mina/bundler'
 require 'mina/git'
+require 'mina/scp'
 
-# Basic settings:
-#   domain       - The hostname to SSH to.
-#   deploy_to    - Path to deploy into.
-#   repository   - Git repo to clone from. (needed by mina/git)
-#   branch       - Branch name to deploy. (needed by mina/git)
+set :shared_paths, [
+  'config/settings.php'
+]
+
+set :app_root, File.expand_path('../../', __FILE__)
 
 set :domain, 'experiments.weber.edu'
 set :deploy_to, '/u/apps/orsee'
@@ -24,10 +24,16 @@ task :setup => :environment do
   queue  %[echo "-----> Be sure to edit 'shared/config/database.yml'."]
 end
 
+desc "Upload secret configuration files."
+task :config => :environment do
+  scp_upload "#{app_root}/config/settings.php", "#{deploy_to}/shared/config/settings.php"
+end
+
 desc "Deploys the current version to the server."
 task :deploy => :environment do
   deploy do
     invoke :'git:clone'
+    invoke :'deploy:link_shared_paths'
 
     to :launch do
       queue "sudo service nginx restart"
