3971237183a2ab585f175fb8dc94a48e04285da5
Jeffrey Horn <jrhorn424@gmail.com>
2014-01-31 17:31:33 -0500

    Update FAQ

--- public/faq.php
+++ public/faq.php
@@ -4,44 +4,61 @@ $menu__area="faqs";
 
 include("header.php");
 
-	echo '
-		<BR><BR>
-		<center>
-			<h4>'.$lang['faq_long'].'</h4>
-		<BR>
-		<TABLE width=70%>
-			<TR>
-				<TD>
-				</TD>
-				<TD width=20% class="small">
-					'.$lang['this_faq_answered_questions_of_xxx'].'
-				</TD>
-			</TR>';
+<h2 id="faqs">FAQs</h2>
 
-     	$query="SELECT * FROM ".table('faqs').", ".table('lang')."
-	     	WHERE ".table('lang').".content_name=".table('faqs').".faq_id
-	     	AND ".table('lang').".content_type='faq_question'
-	     	ORDER BY ".table('faqs').".evaluation DESC, ".table('lang').".".$lang['lang'];
-    	$result=mysql_query($query) or die("Database error: " . mysql_error());
+<p>If your question isn't answered below, please <a href="/contact">contact us</a>.</p>
 
-	$shade=false;
-	while ($line=mysql_fetch_assoc($result)) {
+<h4 id="howwilligetpaid">How will I get paid?</h4>
 
-		if ($shade) $shade=false; else $shade=true;
-  		echo '<TR>
-			<TD>
-        			<A HREF="javascript:popupfaq(\'faq_show.php?id='.$line['faq_id'].'\')">';
-  				if ($shade) echo '<FONT COLOR="#0000FF">'; else echo '<FONT color="#000000">';
-  				echo stripslashes($line[$lang['lang']]);
-  				echo '</FONT></A>
-        		</TD>
-			<TD>
-  				'.$line['evaluation'].' '.$lang['persons'].'
-			</TD>
-		      </TR>';
-		}
-	echo '	</TABLE>
-		</center>';
+<p>If you show up on time, you will receive your on-time bonus (a.k.a. show
+up fee). In addition to that, you will be paid an amount that depends on
+the decisions you and others make during an experiment. You will be paid
+privately in cash at the end of the experiment.</p>
+
+<h4 id="howlongdoesanexperimentusuallylast">How long does an experiment usually last?</h4>
+
+<p>A typical experiment lasts between 30 minutes to 3 hours, depending on the
+purpose of the study. We inform you how long the experiment lasts in each
+of our invitation emails.</p>
+
+<h4 id="imnowregisterednowwhat">I'm now registered. Now what?</h4>
+
+<p>Thank you for registering! You don't need to do anything else for now. When
+researchers begin new experiments, you will start to receive invitation
+emails.</p>
+
+<h4 id="imnotreceivinganyinvitationswhy">I'm not receiving any invitations. Why?</h4>
+
+<p>Typically, not everyone in the database gets an invitation to every
+experiment. However, if you have already participated in an experiment,
+you will not be eligible for a different session of the same experiment.
+Please wait until new experiments are created.</p>
+
+<p>If you are not receiving any invitations at all, our invitations could be
+being sent to your spam folder or you may have entered the wrong email
+address. Go to experiments.weber.edu. Try logging in and double check that
+the email address you registered with is correct.</p>
+
+<h4 id="howcaniunconfirmanexperiment">How can I "unconfirm" an experiment?</h4>
+
+<p>Unfortunately, there is no feature which allows you to "unconfirm" once you
+have RSVP'd for an experiment. However, we understand that emergencies and
+other matters do arise unexpectedly. Please inform the researcher who
+invited you to the experiment as soon as you can.</p>
+
+<h4 id="iamnolongerinterestedinparticipatinginexperimentswhatcanido">I am no longer interested in participating in experiments. What can I do?</h4>
+
+<p>Please go to experiments.weber.edu. Log in to your account and follow the
+instruction there to unsubscribe. Otherwise, please email your name and ID
+number to rongrong at weber dot edu with the subject title, "UNSUBSCRIBE."</p>
+
+<h4 id="ihavesomequestionsconcernsiwantaddressedwhoshouldicontact">I have some questions/concerns I want addressed. Who should I contact?</h4>
+
+<p>If your question or concern is specific to an experiment, contact the
+experimenter who sent you the invitation. If it is about experiments and
+policy in general, contact Dr. Rong Rong at rongrong@weber.edu. Please
+take the time to read the FAQs section before you email her, as your
+question may already be answered.</p>
 
 include ("footer.php");
 ?>
